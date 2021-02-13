24a32eb6f849a08f675be39f3c17a879f323f08b
liulibiao <liulibiao@cvte.com>
2021-02-13 16:03:24 +0800

    style

---
+++ pages/my/agree.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="we">
+		<view class="we-line">
+			<text class="text">《用户协议》</text>
+			<uni-icons color="#555" class="icon" type="arrowright" size="15"></uni-icons>
+		</view>
+		<view class="we-line">
+			<text class="text">《隐私政策》</text>
+			<uni-icons color="#555" class="icon" type="arrowright" size="15"></uni-icons>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+	@import './style.scss';
+</style>
